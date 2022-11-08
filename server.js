a2481cb239ee676274be1d5a99629508a249ff67
takuyadev <takuya.k.toyokawa@protonmail.com>
2022-11-08 15:13:15 -0800

    Fixed issue with index.html

--- server.js
+++ server.js
@@ -1,66 +1,94 @@
-const express = require('express')
-const path = require('path')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const connectDB = require('./config/db')
-const colors = require('colors')
-const errorHandler = require('./middleware/error')
-const fileupload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
 connectDB();
 
 // Route files
-const bootcamps = require('./routes/bootcamps')
-const courses = require('./routes/courses')
-const auth = require('./routes/auth')
-const users = require('./routes/users')
-const reviews = require('./routes/reviews')
-const app = express()
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
-// Body Parser
-app.use(express.json())
+const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-   app.use(morgan('dev'))
+   app.use(morgan('dev'));
 }
 
 // File uploading
-app.use(fileupload())
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+   windowMs: 10 * 60 * 1000, // 10 mins
+   max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollutiona
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
-
-// ? Small example on how to use a logger
-// const logger = (req, res, next) => {
-//    req.hello = "World"
-//    console.log('Middleware Ran')
-//    next()
-// }
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps)
-app.use('/api/v1/courses', courses)
-app.use('/api/v1/auth', auth)
-app.use('/api/v1/users', users)
-app.use('/api/v1/reviews', reviews)
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold))
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+   PORT,
+   console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+   )
+);
 
 // Handle unhandled promise rejections
-
 process.on('unhandledRejection', (err, promise) => {
-   console.log(`Error ${err.message}`.red);
+   console.log(`Error: ${err.message}`.red);
    // Close server & exit process
-   server.close(() => process.exit(1))
-})
-
+   // server.close(() => process.exit(1));
+});
